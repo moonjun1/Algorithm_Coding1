64bb80c997d6053c7b03ba8fa2c66c2eab008d88
moon <152607052+moonjun1@users.noreply.github.com>
2025-05-09 11:22:01 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 172 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        boolean[] submit = new boolean[30];
+
+        for (int i = 0; i < 28; i++) {
+            int n = sc.nextInt();
+            submit[n - 1] = true;
+        }
+
+        for (int i = 0; i < 30; i++) {
+            if(!submit[i]) {
+                System.out.println(i + 1);
+            }
+        }
+    }
+}
