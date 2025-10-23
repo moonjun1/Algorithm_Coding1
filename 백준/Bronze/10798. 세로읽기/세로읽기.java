33565e65ac32bfe07c6852b2e3c62ad13b5a33c9
moon <152607052+moonjun1@users.noreply.github.com>
2025-10-23 16:41:44 +0900

    [Bronze I] Title: 세로읽기, Time: 172 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        // 5줄 입력 받기
+        String[] words = new String[5];
+        for (int i = 0; i < 5; i++) {
+            words[i] = sc.nextLine();
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(int col = 0; col < 15; col++) {
+            for(int row = 0; row < 5; row++) {
+                if(col < words[row].length()) {
+                    result.append(words[row].charAt(col));
+                }
+            }
+        }
+
+        System.out.println(result);
+
+    }
+}
